3ae69d2611eb9700ca3103725cc9534e37ab1bc2
Joushva Kamble <kamble.joushva@gmail.com>
2024-07-29 12:51:31 -0400

    Remove an argument that checks if model is a transfer learning model

--- Colonoscopy Medical Annotation Project/modular/train.py
+++ Colonoscopy Medical Annotation Project/modular/train.py
@@ -35,11 +35,11 @@ parser = argparse.ArgumentParser(description="Train a PyTorch multiclassificatio
 parser.add_argument("--num_epochs", type=int, default=20, help="Number of epochs to train the model. Default is 20.")
 parser.add_argument("--batch_size", type=int, default=32, help="Number of samples per batch. Default is 32.")
 parser.add_argument("--learning_rate", type=float, default=0.005, help="Learning rate for the optimizer. Default is 0.005.")
-parser.add_argument("--hidden_units", type=int, required=True, help="Number of hidden units in the model. Argument is required.")
+parser.add_argument("--hidden_units", type=int, default=10, help="Number of hidden units in the model. Default is 10. Not needed for transfer learning models.")
 
 parser.add_argument("--model_path", type=str, required=True, help=f"Path to the model file. Argument is required. Available models: {list_models()}")
 # TODO: Check to see if this argument is needed
-parser.add_argument("--transfer_learning", action="store_true", help="Indicate if the model is a transfer learning model.")
+# parser.add_argument("--transfer_learning", action="store_true", help="Indicate if the model is a transfer learning model.")
 
 # Parse the arguments
 args = parser.parse_args()
