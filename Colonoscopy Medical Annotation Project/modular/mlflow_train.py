97501831bcbf0c9729864f87080a1ffdfeb18d54
Joushva Kamble <kamble.joushva@gmail.com>
2024-07-31 13:51:46 -0400

    Made some changes in mlflow_train.py file for the create and set experiment functions

--- Colonoscopy Medical Annotation Project/modular/mlflow_train.py
+++ Colonoscopy Medical Annotation Project/modular/mlflow_train.py
@@ -158,8 +158,8 @@ experiment = mlflow.get_experiment_by_name(experiment_name)
 
 if experiment is None:
     # Create the experiment and set it
-    exp_id = mlflow.create_experiment(experiment_name)
-    mlflow.set_experiment(exp_id)
+    mlflow.create_experiment(experiment_name)
+    mlflow.set_experiment(experiment_name)
 else:
     # Set the experiment
     mlflow.set_experiment(experiment_name)
