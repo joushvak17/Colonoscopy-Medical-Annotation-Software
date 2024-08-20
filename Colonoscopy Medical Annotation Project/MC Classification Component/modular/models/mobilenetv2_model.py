12d75a696abb0ed980fb16738769ed06899d88ae
Joushva Kamble <kamble.joushva@gmail.com>
2024-08-20 11:23:44 -0400

    Created a new MobileNetV2 model

---
+++ Colonoscopy Medical Annotation Project/MC Classification Component/modular/models/mobilenetv2_model.py
@@ -0,0 +1,34 @@
+"""
+Defines a PyTorch model that replicates the MobileNetV2 architecture.
+"""
+import torch
+from torch import nn
+import torchvision.models as models
+
+class MobileNetV2(nn.Module):
+    def __init__(self, output_shape: int, device=None) -> None:
+        """Defines a MobileNetV2 model using transfer learning.
+
+        Args:
+            output_shape (int): The output shape of the model.
+            device (_type_, optional): The device to run the model on. Defaults to None.
+        """
+        super().__init__()
+        self.weights = models.MobileNet_V2_Weights.DEFAULT
+        self.model = models.mobilenet_v2(weights=self.weights).to(device)
+        
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # Unfreeze the last 7 layers
+        for param in self.model.features[-7:].parameters():
+            param.requires_grad = True
+        
+        num_ftrs = self.model.classifier[1].in_features
+        self.model.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(num_ftrs, output_shape)
+        ).to(device)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
