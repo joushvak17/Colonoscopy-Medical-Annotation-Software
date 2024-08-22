3e6e571923a387eb87f2c4d66c3b463b336136c8
Joushva Kamble <kamble.joushva@gmail.com>
2024-08-22 15:20:57 -0400

    Added a TODO statement

--- Machine Learning Components/Object Detection & Tracking Component/data_download.py
+++ Machine Learning Components/Object Detection & Tracking Component/data_download.py
@@ -1,4 +1,3 @@
-import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 # Initialize the Kaggle API
@@ -8,3 +7,5 @@ api.authenticate()
 # Download the dataset for the object detection/tracking component
 dataset_name = "kelkalot/the-hyper-kvasir-dataset"
 api.dataset_download_files(dataset_name, path=".", force=True, quiet=False, unzip=True)
+
+# TODO: The full dataset is too large to be downloaded in the current environment. The required dataset has been locally downloaded and uploaded to the environment.
